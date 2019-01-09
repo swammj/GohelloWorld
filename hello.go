90999886cb7066721027fc02260d66fd0a21baef
swammj <swamyjose@unitec.edu>
2019-01-09 03:21:17 -0600

    short declarations

--- hello.go
+++ hello.go
@@ -3,6 +3,20 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("AJAJAJA")
-	fmt.Println("Hello razy Fucking World")
+	fmt.Println("Hello Crazy Fucking World")
+	foo()
+	for i := 0 ;i < 15; i++ {
+		if i%2 == 0 {
+			fmt.Println(i)
+		}
+	}
 }
+
+func foo() {
+	fmt.Println("Im in fooo")
+}
+
+//control flow
+//(1) sequence
+//(2) loop; iterative
+//(3) conditional
---
+++ short-declaration.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	x := 34
+	fmt.Println(x)
+	x = 93
+	fmt.Println(x)
+	xq := "Hola"
+	fmt.Println(xq, x)
+}
